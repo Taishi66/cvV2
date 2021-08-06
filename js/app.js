8629778fd61f2053e79ef1d44f77d67366451598
Taishi <jctlamy@gmail.com>
2021-08-06 14:39:18 +0200

    add responsive

--- js/app.js
+++ js/app.js
@@ -28,7 +28,40 @@ $(document).ready(function() {
         prevScrollpos = currentScrollPos;
     }
 
-    //zoom in photo onclick
+    //fade in section
+    const faders = document.querySelectorAll('.fade-in');
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -250px 0px"
+    };
 
+    const appearOnScroll = new IntersectionObserver(function(
+        entries,
+        appearOnScroll,
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
 
-})
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+
+    //burger menu 
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menuBurger");
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+
+});
